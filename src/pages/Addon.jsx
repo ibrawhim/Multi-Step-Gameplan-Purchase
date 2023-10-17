449b9e6db45a573b0e7763c9db4d3f01d15a7fe1
Adeniyi Ibrahim Abiodun <ibrahimabiodun069@gmail.com>
2023-10-17 14:27:08 +0100

    multi

--- src/pages/Addon.jsx
+++ src/pages/Addon.jsx
@@ -1,9 +1,107 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { handleNextStep, handlePreviousStep } from '../redux/plan'
+import { useDispatch, useSelector } from 'react-redux'
 
-const AddOn = () => {
-  return (
-    <div>AddOn</div>
-  )
+
+const Addon = () => {
+    // const [FormData, setFormData] = useState(second)
+    const [addOn, setAddOn] = useState({
+        addOnTypes: [],
+        isYearPlanLength: false
+    })
+
+    const store = useSelector(state => state.planData)
+    // console.log(store);
+    const available = ([
+        {
+            name: 'Online services',
+            desc: 'Access to multiplayer games',
+            monthly: 1,
+            yearly: 10,
+            id: 1
+        },
+        {
+            name: 'Larger storage',
+            desc: 'Extra 1TB of cloud save',
+            monthly: 2,
+            yearly: 20,
+            id: 2
+        }, {
+            name: 'Customizable profile',
+            desc: 'Custom theme on your profile',
+            monthly: 2,
+            yearly: 20,
+            id: 3
+        },
+    ])
+
+    const dispatch = useDispatch()
+
+    useEffect(() => {
+        setAddOn((prev) => ({ ...store }))
+    }, [])
+
+    const handlePrevious = () => {
+        dispatch(handlePreviousStep())
+    }
+
+    const handleSubmit = () => {
+        const { addOnTypes } = addOn
+        if (addOnTypes) {
+            dispatch(handleNextStep(addOn))
+        }
+    }
+
+
+    const handleChange = (val, id) => {
+
+        const exist = addOn.addOnTypes?.find((ad) => ad?.id === id)
+
+        if (exist) {
+            setAddOn({
+                ...addOn, addOnTypes: [...addOn?.addOnTypes?.filter(type => type.id !== id)]
+            })
+        } else {
+            setAddOn({
+                ...addOn, addOnTypes: [...addOn.addOnTypes, { ...val }]
+            })
+
+        }
+
+    }
+    return (
+        <div>
+            <h1 className='text-2xl  font-bold font-sans'>Pick add-ons</h1>
+            <p className='text-xl font-sans'>Add-ons help enhance your gaming experience.</p>
+            {
+                available.map(item => (
+                    <label key={item.id} htmlFor={`addon${item.id}`}>
+                        <div className={`addOnButton flex justify-between border items-center mt-6 py-2 rounded-xl ${addOn?.addOnTypes?.some(val => val.id === item.id) && 'active'}`}>
+
+
+                            <div className="flex">
+                                <input type="checkbox" className='ml-10' checked={addOn.addOnTypes.some(val => val.id === item.id)} id={`addon${item.id}`}
+                                    name="addOn" onChange={(e) => handleChange(item, item.id)} />
+                            </div>
+
+                            <div className="addOn">
+                                <p className="AddSize">{item.name}</p>
+                                <span>{item.desc}</span>
+                            </div>
+                            <p className="mr-10">+${addOn?.isYearPlanLength ? item.yearly : item.monthly}/ {!addOn?.isYearPlanLength ? "mo" : "yr"}</p>
+                        </div>
+                    </label>
+                ))
+            }
+
+            <div className='lg:flex justify-center flex lg:gap-0 gap-[120px] mt-10'>
+                <button className='text-xl' onClick={handlePrevious}>Back</button>
+                <button onClick={handleSubmit} className='bg-blue-800 text-white rounded lg:ms-96 ms-26 lg:mt-0 lg:p-3 p-2 lg:w-[25%] w-[105px]'>CONFIRM</button>
+            </div>
+        </div>
+
+
+    )
 }
 
-export default AddOn
+export default Addon
--- src/pages/Selectplan.jsx
+++ src/pages/Selectplan.jsx
@@ -70,10 +70,10 @@ const Selectplan = () => {
         <div>
             <h1 className='text-2xl font-bold lg:py-4 py-2'>Select your plan</h1>
             <p>You have the option of monthly or yearly billing.</p>
-            <div className='grid lg:grid-cols-3 gap-2 md:grid-cols-4 grid-cols-1'>
+            <div className='grid lg:grid-cols-3 lg:gap-6 lg:w-[430px] gap-2  grid-cols-1'>
               {
                 availablePlan.map((item,i)=>(
-                      <button key={i} className='border flex lg:flex-col lg:gap-0 gap-5 lg:h-[160px] h-[70px] w-full lg:w-[120px] rounded-xl px-2' onClick={() => updateForm(i)}>
+                      <button key={i} className={`border flex lg:flex-col lg:gap-0 lg:h-[160px] cursor-pointer h-[70px] w-full lg:w-full rounded-xl px-2 hover:border-black ${item.name === formData.planType ? 'border-black border-1' : ''}` } onClick={() => updateForm(i)}>
                         <div className={item.bg} style={{marginTop: '12px', marginLeft: '5px', borderRadius: '100%', padding: '10px'}}>{item.image}</div>
                         <div className='lg:mt-6'>
                           <div className='flex'>{item.name}</div>
@@ -86,7 +86,7 @@ const Selectplan = () => {
             </div>
 
 
-            <div className='flex justify-center items-center w-100 h-[56px] mt-[25px] rounded-xl bg-sky-100'>
+            <div className='flex justify-center items-center lg:w-[430px] w-full h-[56px] mt-[25px] rounded-xl bg-sky-100'>
                     <p className='mt-3'>Monthly</p>
                     <label className='check w-[40px] h-[20px] cursor-pointer inline-block relative overflow-hidden mt-4 mr-2.5 ml-2.5 '>
                         <input type="checkbox" checked={formData.isYearPlanLength} onChange={(e) => setFormData({ ...formData, isYearPlanLength: e.target.checked, planType: '', planAmount: 0 })} className='absolute top-[-30px] left-[-30px] w-0 h-0' id='checkbox' name='planLength' />
